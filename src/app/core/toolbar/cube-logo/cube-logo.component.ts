bc9ab6469307f886e823d724a017146f75877810
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-04 18:25:32 -0400

    chore: Add cube logo component files and styles

---
+++ src/app/core/toolbar/cube-logo/cube-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cube-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './cube-logo.component.html',
+  styleUrl: './cube-logo.component.scss'
+})
+export class CubeLogoComponent {
+
+}
