09d71b32e4b1bbde8db7bf23e6a7d6037180c56e
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-04 18:14:07 -0400

    chore: Add toolbar component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './core/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ToolbarComponent],
 })
 export class AppComponent {
   title = 'angular-net-connect';
