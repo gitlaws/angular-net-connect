0b1ae34f219cca838b129dfc12bdf9cedf47f31d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-05 13:43:16 -0400

    chore: Add cyber menu component for improved navigation
    Add the cyber menu component to the toolbar component's HTML to provide an improved navigation experience. The cyber menu includes links to projects and profile sections, enhancing the accessibility and usability of the application.

---
+++ src/app/core/cyber-menu/cyber-menu.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnDestroy, ElementRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cyber-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './cyber-menu.component.html',
+  styleUrl: './cyber-menu.component.scss',
+})
+export class CyberMenuComponent {
+  isMenuOpen = false;
+  menuItems = [
+    { name: 'Projects', url: '/home' },
+    { name: 'Profile', url: '/about' },
+  ];
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnDestroy(): void {
+    // Remove the click event listener from the document
+    document.removeEventListener('click', this.documentClickHandler.bind(this));
+  }
+
+  documentClickHandler(event: MouseEvent) {
+    // Check if the click event is coming from within the dropdown content
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      // If not, close the dropdown
+      this.isMenuOpen = false;
+    }
+  }
+
+  toggleMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CubeLogoComponent } from './cube-logo/cube-logo.component';
+import { CyberMenuComponent } from '../cyber-menu/cyber-menu.component';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
-  imports: [CubeLogoComponent],
+  imports: [CubeLogoComponent, CyberMenuComponent],
 })
 export class ToolbarComponent {}
