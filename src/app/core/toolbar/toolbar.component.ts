d7631854b82345db3e4d5ffb30f2b382c218c123
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-05 13:46:20 -0400

    chore: Import RouterLink for improved navigation in toolbar component

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CubeLogoComponent } from './cube-logo/cube-logo.component';
 import { CyberMenuComponent } from '../cyber-menu/cyber-menu.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
-  imports: [CubeLogoComponent, CyberMenuComponent],
+  imports: [CubeLogoComponent, CyberMenuComponent, RouterLink],
 })
 export class ToolbarComponent {}
