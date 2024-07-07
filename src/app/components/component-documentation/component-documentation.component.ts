0797b06d5972a68b7e6a211276d0bb8e8e0a5bab
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-07 14:51:58 -0400

    Update card component to use grid layout for improved styling and structure

---
+++ src/app/components/component-documentation/component-documentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-documentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-documentation.component.html',
+  styleUrl: './component-documentation.component.scss'
+})
+export class ComponentDocumentationComponent {
+
+}
