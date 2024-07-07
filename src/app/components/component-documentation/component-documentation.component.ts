2379f3654c35378183c80cfa4f4aa5583f3317f5
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-07 15:08:50 -0400

    refactor: Update component-documentation to use app-card and CommonModule for improved styling and structure

--- src/app/components/component-documentation/component-documentation.component.ts
+++ src/app/components/component-documentation/component-documentation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-component-documentation',
   standalone: true,
   templateUrl: './component-documentation.component.html',
   styleUrl: './component-documentation.component.scss',
-  imports: [CardComponent],
+  imports: [CommonModule, CardComponent],
 })
 export class ComponentDocumentationComponent {}
