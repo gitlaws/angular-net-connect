9e68debfe6046886d0c7fff77864932e829984a0
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-07 15:04:43 -0400

    refactor: Update component-documentation to use app-card for improved styling and structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   {
+  //     path: '',
+  //     loadComponent: () =>
+  //       import('./core/home/home.component').then((m) => m.HomeComponent),
+  //   },
+
+  {
+    path: 'Component-Documentation',
+    loadComponent: () =>
+      import(
+        './components/component-documentation/component-documentation.component'
+      ).then((m) => m.ComponentDocumentationComponent),
+  },
+];
