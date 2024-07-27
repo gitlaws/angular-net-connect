827dcc101c348fa05fe03c4f5f2131a3f75bdeaa
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-26 21:40:34 -0400

    refactor: Update cyber-menu component to toggle menu visibility and handle document click events

--- src/app/core/cyber-menu/cyber-menu.component.ts
+++ src/app/core/cyber-menu/cyber-menu.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, ElementRef } from '@angular/core';
+import { Component, OnDestroy, ElementRef, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,27 +9,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './cyber-menu.component.html',
   styleUrl: './cyber-menu.component.scss',
 })
-export class CyberMenuComponent {
+export class CyberMenuComponent implements OnDestroy {
   isMenuOpen = false;
-  menuItems = [
-    { name: 'Projects', url: '/home' },
-    { name: 'Profile', url: '/about' },
-  ];
 
   constructor(private elementRef: ElementRef) {}
 
   ngOnDestroy(): void {
-    document.removeEventListener('click', this.documentClickHandler.bind(this));
-  }
-
-  documentClickHandler(event: MouseEvent) {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.isMenuOpen = false;
-    }
+    this.removeDocumentClickListener();
   }
 
   toggleMenu(event: MouseEvent) {
     event.stopPropagation();
     this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.addDocumentClickListener();
+    } else {
+      this.removeDocumentClickListener();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  documentClickHandler(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isMenuOpen = false;
+      this.removeDocumentClickListener();
+    }
+  }
+
+  private addDocumentClickListener() {
+    document.addEventListener('click', this.documentClickHandler.bind(this));
+  }
+
+  private removeDocumentClickListener() {
+    document.removeEventListener('click', this.documentClickHandler.bind(this));
   }
 }
