154225b1ab3a374301fc62b6a4d8f54fff097003
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-28 16:51:21 -0400

    refactor: Update accordion component to use ngFor and add expand/collapse functionality

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccordionItem } from './accordion-item.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accordion.component.html',
-  styleUrl: './accordion.component.scss'
+  styleUrl: './accordion.component.scss',
 })
 export class AccordionComponent {
+  @Input() public items: AccordionItem[] = [];
 
+  public toggle(item: AccordionItem): void {
+    item.isExpanded = !item.isExpanded;
+  }
 }
