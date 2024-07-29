045f57cd8ef09a2faa959fdcbc28071222a1495e
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-29 18:32:43 -0400

    refactor: Improve snackbar component and update service-documentation with snackbar functionality

--- src/app/components/snackbar/snackbar.component.ts
+++ src/app/components/snackbar/snackbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { SnackbarService } from '../../services/snackbar/snackbar.service';
