59379f6530c5b9c0f820acbef5e99d8f84c10902
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-29 18:26:54 -0400

    refactor: Add Snackbar component and update service-documentation with Snackbar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
 import { ToolbarComponent } from './core/toolbar/toolbar.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'], // Corrected from 'styleUrl' to 'styleUrls' and made it an array
-  imports: [RouterOutlet, ToolbarComponent, ComponentDocumentationComponent],
+  imports: [
+    RouterOutlet,
+    ToolbarComponent,
+    ComponentDocumentationComponent,
+    SnackbarComponent,
+  ],
 })
 export class AppComponent {
   title = 'angular-net-connect';
--- src/app/services/service-documentation/service-documentation.component.ts
+++ src/app/services/service-documentation/service-documentation.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SnackbarService } from '../../services/snackbar/snackbar.service';
+import { CardComponent } from '../../components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { SnackbarComponent } from '../../components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-service-documentation',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, RouterLink, SnackbarComponent],
   templateUrl: './service-documentation.component.html',
-  styleUrl: './service-documentation.component.scss'
+  styleUrls: ['./service-documentation.component.scss'],
 })
 export class ServiceDocumentationComponent {
+  constructor(private snackbarService: SnackbarService) {}
 
+  public callSnackbar(): void {
+    this.snackbarService.callSnackbar('Snackbar Service Example');
+  }
 }
