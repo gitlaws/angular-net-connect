25f1e72f5ccc6f88e862cd2bafa7c7dcbcd8a75f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-29 15:05:16 -0400

    refactor: Update API documentation component with static content

--- src/app/apis/api-documentation/api-documentation.component.ts
+++ src/app/apis/api-documentation/api-documentation.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-api-documentation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CardComponent],
   templateUrl: './api-documentation.component.html',
-  styleUrl: './api-documentation.component.scss'
+  styleUrl: './api-documentation.component.scss',
 })
-export class ApiDocumentationComponent {
-
-}
+export class ApiDocumentationComponent {}
