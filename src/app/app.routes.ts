cd06d76bdf3600f7bef95ccb7a215b2d86b4f509
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-29 14:48:30 -0400

    refactor: Remove unused services-view component and replace with service-documentation component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,30 +19,11 @@ export const routes: Routes = [
       ).then((component) => component.ComponentDocumentationComponent),
   },
 
-  // Assuming components exist for these paths, add them similarly
-  // {
-  // path: 'services',
-  // loadChildren: () =>
-  //   import('./services/services.component').then((s) => s.ServicesComponent),
-  // },
-  // {
-  // path: 'pipes',
-  // loadChildren: () =>
-  //   import('./pipes/pipes.component').then((p) => p.PipesComponent),
-  // },
-  // {
-  // path: 'directives',
-  // loadChildren: () =>
-  //   import('./directives/directives.component').then((d) => d.DirectivesComponent),
-  // },
-  // {
-  // path: 'apis',
-  // loadChildren: () =>
-  //   import('./apis/apis.component').then((a) => a.ApisComponent),
-  // },
-  // {
-  // path: 'other',
-  // loadChildren: () =>
-  //   import('./other/other.component').then((o) => o.OtherComponent),
-  // },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import(
+        './services/service-documentation/service-documentation.component'
+      ).then((component) => component.ServiceDocumentationComponent),
+  },
 ];
