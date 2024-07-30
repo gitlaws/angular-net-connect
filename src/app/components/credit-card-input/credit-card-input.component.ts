4a8778b2971a41cc9e8d442e5141d9b3e7f1773f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-30 14:00:39 -0400

    refactor: Update credit card input component and authentication state in component documentation

--- src/app/components/credit-card-input/credit-card-input.component.ts
+++ src/app/components/credit-card-input/credit-card-input.component.ts
@@ -1,20 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthenticatorService } from '../../services/authenticator/authenticator.service';
 
 @Component({
   selector: 'app-credit-card-input',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './credit-card-input.component.html',
-  styleUrl: './credit-card-input.component.scss',
+  styleUrls: ['./credit-card-input.component.scss'],
 })
 export class CreditCardInputComponent {
   @Input() public creditCardNumber = '';
   private readonly = false;
+
+  constructor(private authenticatorService: AuthenticatorService) {}
+
   public get isReadonly() {
     return this.readonly;
   }
+
   @Input() public set isReadonly(value: boolean) {
     this.creditCardNumber = value
       ? this.formatReadonlyCCNum(this.creditCardNumber)
@@ -33,5 +38,11 @@ export class CreditCardInputComponent {
     });
 
     return onlyLast4Shown.join('-');
+  }
+
+  public isValidCard(): boolean {
+    return this.authenticatorService.validateCreditCardNumber(
+      this.creditCardNumber
+    );
   }
 }
---
+++ src/app/services/authenticator/authenticator.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticatorService {
+  constructor() {}
+
+  validateCreditCardNumber(creditCardNumber: string): boolean {
+    const iin = creditCardNumber.slice(0, 6);
+    const mii = creditCardNumber[0];
+    const bin = creditCardNumber.slice(1, 6);
+
+    if (mii === '3' && (iin.startsWith('34') || iin.startsWith('37'))) {
+      return true; // American Express
+    } else if (mii === '4' && /^[2-6]/.test(bin)) {
+      return true; // Visa
+    } else if (mii === '5' && /^[2-6]/.test(bin)) {
+      return true; // Mastercard
+    } else if (mii === '6' && /^[2-6]/.test(bin)) {
+      return true; // Discover
+    }
+
+    return false;
+  }
+}
