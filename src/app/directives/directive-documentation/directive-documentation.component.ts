07529d70c25bab2ecbea14d184d7dad37b117dc2
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-30 19:02:14 -0400

    refactor: Add copy button styling to directive documentation

--- src/app/directives/directive-documentation/directive-documentation.component.ts
+++ src/app/directives/directive-documentation/directive-documentation.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CardComponent } from '../../components/card/card.component';
-import { CopyDirective } from '../copy/copy.directive'; // Import the CopyDirective
+import { CopyDirective } from '../copy/copy.directive';
 
 @Component({
   selector: 'app-directive-documentation',
   standalone: true,
-  imports: [CommonModule, RouterLink, CardComponent, CopyDirective], // Add CopyDirective to imports
+  imports: [CommonModule, RouterLink, CardComponent, CopyDirective],
   templateUrl: './directive-documentation.component.html',
   styleUrls: ['./directive-documentation.component.scss'],
 })
