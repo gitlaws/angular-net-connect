aef1dec9250ca1fb47de5cd2e39d4f87e51684be
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-01 12:33:16 -0400

    refactor: Add Input decorator to LoaderComponent

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoaderType } from './models/loader-type.enum';
 import { CommonModule } from '@angular/common';
 
