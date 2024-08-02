06c99fef0da7b85e3ce5ce97f95dee487ef6e70a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-02 16:28:09 -0400

    refactor: Remove app-footer from app.component.html and add it to home.component.html

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
