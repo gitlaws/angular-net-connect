5b71e0f8c3fb3555fa181c07fc826c4ef94b8319
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-04 18:33:04 -0400

    refactor: Remove flatten-array component and related files

--- src/app/pipes/flatten/flatten.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FlattenPipe } from './flatten.pipe';
-
-describe('FlattenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FlattenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { CreditCardFormatterPipe } from '../credit-card-formatter/credit-card-formatter.pipe';
-import { FlattenPipe } from '../flatten/flatten.pipe';
-import { FlattenArrayComponent } from '../flatten-array/flatten-array.component';
 
 @Component({
   selector: 'app-pipe-documentation',
   standalone: true,
-  imports: [
-    CardComponent,
-    CreditCardFormatterPipe,
-    FlattenPipe,
-    FlattenArrayComponent,
-  ],
+  imports: [CardComponent, CreditCardFormatterPipe],
   templateUrl: './pipe-documentation.component.html',
   styleUrls: ['./pipe-documentation.component.scss'],
 })
