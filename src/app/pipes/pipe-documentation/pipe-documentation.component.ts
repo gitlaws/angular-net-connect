eb2d9b86eaca977033cec6631421079c066ed0ba
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-03 21:05:18 -0400

    refactor: Add app-card component to pipe-documentation

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-pipe-documentation',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './pipe-documentation.component.html',
-  styleUrl: './pipe-documentation.component.scss'
+  styleUrl: './pipe-documentation.component.scss',
 })
-export class PipeDocumentationComponent {
-
-}
+export class PipeDocumentationComponent {}
