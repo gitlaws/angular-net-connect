3615da99e530138b8f57c34d17f155c9a8548603
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-04 18:09:06 -0400

    refactor: Add flatten pipe for array flattening

---
+++ src/app/pipes/flatten/flatten.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'flatten',
+  standalone: true,
+})
+export class FlattenPipe implements PipeTransform {
+  public transform(values: any[]): any[] {
+    return this.flattenArray(values);
+  }
+
+  private flattenArray(arr: any[]): any[] {
+    const result: any[] = [];
+
+    arr.forEach((value) => {
+      if (Array.isArray(value)) {
+        result.push(...this.flattenArray(value));
+      } else {
+        result.push(value);
+      }
+    });
+
+    return result;
+  }
+}
--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { CreditCardFormatterPipe } from '../credit-card-formatter/credit-card-formatter.pipe';
+import { FlattenPipe } from '../flatten/flatten.pipe';
 
 @Component({
   selector: 'app-pipe-documentation',
   standalone: true,
-  imports: [CardComponent, CreditCardFormatterPipe],
+  imports: [CardComponent, CreditCardFormatterPipe, FlattenPipe],
   templateUrl: './pipe-documentation.component.html',
   styleUrl: './pipe-documentation.component.scss',
 })
-export class PipeDocumentationComponent {}
+export class PipeDocumentationComponent {
+  flattenData: any;
+  modifyFlatten() {
+    throw new Error('Method not implemented.');
+  }
+  reassignFlatten() {
+    throw new Error('Method not implemented.');
+  }
+}
