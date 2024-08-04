4f73b3196af1916d64531c9530731c919301c11f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-04 17:53:50 -0400

    refactor: Update pipe-documentation component with new pipe cards

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { CreditCardFormatterPipe } from '../credit-card-formatter/credit-card-formatter.pipe';
-import { CustomDateFormatterPipe } from '../custom-date-formatter/custom-date-formatter.pipe';
 
 @Component({
   selector: 'app-pipe-documentation',
   standalone: true,
-  imports: [CardComponent, CreditCardFormatterPipe, CustomDateFormatterPipe],
+  imports: [CardComponent, CreditCardFormatterPipe],
   templateUrl: './pipe-documentation.component.html',
   styleUrl: './pipe-documentation.component.scss',
 })
