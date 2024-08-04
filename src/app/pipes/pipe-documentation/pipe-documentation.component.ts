9d7b52a89012301ce63fddf41d80e9f530fd2743
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-04 17:31:45 -0400

    refactor: Add credit card formatter pipe

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { CreditCardFormatterPipe } from '../credit-card-formatter/credit-card-formatter.pipe';
 
 @Component({
   selector: 'app-pipe-documentation',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CreditCardFormatterPipe],
   templateUrl: './pipe-documentation.component.html',
   styleUrl: './pipe-documentation.component.scss',
 })
