97335dd1f2f4c01c644d57fbb3872486d4a96ba8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-09 13:50:12 -0400

    refactor: Update infinite scroll component UI and functionality

--- src/app/components/infinite-scroll/infinite-scroll.component.ts
+++ src/app/components/infinite-scroll/infinite-scroll.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-infinite-scroll',
@@ -7,26 +7,4 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
   templateUrl: './infinite-scroll.component.html',
   styleUrl: './infinite-scroll.component.scss',
 })
-export class InfiniteScrollComponent {
-  @ViewChild('iconContainer', { static: false }) iconContainer!: ElementRef;
-
-  ngAfterViewInit(): void {
-    this.startScrolling();
-  }
-
-  startScrolling(): void {
-    const container = this.iconContainer.nativeElement;
-    const firstChild = container.firstElementChild;
-
-    const scroll = () => {
-      container.scrollLeft += 1;
-      if (container.scrollLeft >= firstChild.clientWidth) {
-        container.appendChild(firstChild);
-        container.scrollLeft = 0;
-      }
-      requestAnimationFrame(scroll);
-    };
-
-    scroll();
-  }
-}
+export class InfiniteScrollComponent {}
