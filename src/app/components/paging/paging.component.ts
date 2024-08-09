dbd177d95405ba7dcc87ee40db319a531f310116
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-09 15:22:30 -0400

    refactor: Update paging component UI and functionality

--- src/app/components/paging/paging.component.ts
+++ src/app/components/paging/paging.component.ts
@@ -1,66 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-paging',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './paging.component.html',
-  styleUrl: './paging.component.scss',
+  styleUrls: ['./paging.component.scss'],
 })
-export class PagingComponent {
-  @Input() public totalItems = 1;
-  @Input() public pageSize = 25;
-  @Input() public currentPage = 1;
-  @Input() public step = 2;
-  @Output() public pageChange: EventEmitter<number> = new EventEmitter();
+export class PagingComponent implements OnInit {
+  @Input() pageSize: number = 10;
+  @Input() totalItems: number = 0;
+  @Input() currentPage: number = 1;
+  @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
 
-  public get totalPages(): number {
-    return Math.ceil((this.totalItems || 1) / (this.pageSize || 1));
+  isLoading = false;
+  images: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchImages();
   }
 
-  public get isFirstPage(): boolean {
-    return this.currentPage === 1;
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
   }
 
-  public isLastPage(): boolean {
-    return this.currentPage === this.totalPages;
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
   }
 
-  public get isPreviousDisabled(): boolean {
-    return this.currentPage - this.step < 1;
+  setPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages && this.currentPage !== page) {
+      this.isLoading = true;
+      this.currentPage = page;
+      this.pageChange.emit(this.currentPage);
+      this.fetchImages();
+    }
   }
 
-  public get isNextDisabled(): boolean {
-    return this.currentPage + this.step > this.totalPages;
-  }
-
-  public get pages(): number[] {
-    const pages = [];
-
+  previousPage(): void {
     if (this.currentPage > 1) {
-      pages.push(this.currentPage - 1);
+      this.setPage(this.currentPage - 1);
     }
-
-    pages.push(this.currentPage);
-
-    if (this.currentPage < this.totalPages) {
-      pages.push(this.currentPage + 1);
-    }
-
-    if (this.currentPage + 1 < this.totalPages) {
-      pages.push(this.currentPage + 2);
-    }
-
-    if (this.currentPage + 1 < this.totalPages && this.currentPage === 1) {
-      pages.push(this.currentPage + 3);
-    }
-
-    return pages;
   }
 
-  public setPage(page: number): void {
-    this.currentPage = page;
-    this.pageChange.emit(page);
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.setPage(this.currentPage + 1);
+    }
+  }
+
+  fetchImages(): void {
+    const apiUrl = `https://api.unsplash.com/photos?page=${this.currentPage}&per_page=${this.pageSize}&client_id=YOUR_ACCESS_KEY`;
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.images = data;
+      this.isLoading = false;
+    });
   }
 }
