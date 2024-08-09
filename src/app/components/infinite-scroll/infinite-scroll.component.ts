c296948bd05a155e335ebac5a3b3cabd368f111c
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-09 12:57:41 -0400

    refactor: Update component documentation to include 404 Not Found component

---
+++ src/app/components/infinite-scroll/infinite-scroll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infinite-scroll',
+  standalone: true,
+  imports: [],
+  templateUrl: './infinite-scroll.component.html',
+  styleUrl: './infinite-scroll.component.scss'
+})
+export class InfiniteScrollComponent {
+
+}
