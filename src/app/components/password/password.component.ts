94a7713dfd0c732c41c7545e165792efff37d540
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-09 22:44:28 -0400

    refactor: Improve password component HTML, styling, and documentation

--- src/app/components/password/password.component.ts
+++ src/app/components/password/password.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], // Add FormsModule to imports array
   templateUrl: './password.component.html',
-  styleUrl: './password.component.scss'
+  styleUrls: ['./password.component.scss'],
 })
 export class PasswordComponent {
+  public passwordStrengthPercent = 0;
 
+  private currentPassword = '';
+
+  public set password(value: string) {
+    this.currentPassword = value;
+    this.updatePasswordStrength(value);
+  }
+
+  public get password(): string {
+    return this.currentPassword;
+  }
+
+  public get is8CharactersOrGreater(): boolean {
+    return this.currentPassword.length >= 8;
+  }
+
+  public get hasOneCapitalLetterOrMore(): boolean {
+    return /(.*[A-Z].*)/.test(this.currentPassword);
+  }
+
+  public get hasOneNumberOrMore(): boolean {
+    return /(.*\d.*)/.test(this.currentPassword);
+  }
+
+  public get hasOneSpecialCharacterOrMore(): boolean {
+    return /\W|_/g.test(this.currentPassword);
+  }
+
+  private updatePasswordStrength(value: string): void {
+    let passwordStrength = 0;
+
+    if (this.is8CharactersOrGreater) {
+      passwordStrength += 25;
+    }
+
+    if (this.hasOneCapitalLetterOrMore) {
+      passwordStrength += 25;
+    }
+
+    if (this.hasOneNumberOrMore) {
+      passwordStrength += 25;
+    }
+
+    if (this.hasOneSpecialCharacterOrMore) {
+      passwordStrength += 25;
+    }
+
+    this.passwordStrengthPercent = passwordStrength;
+  }
 }
