9ee75437de84da339ec984d14c50a1e713e3bdad
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-15 15:40:08 -0400

    refactor: Update API documentation component to use CRUD Nest component

--- src/app/apis/api-documentation/api-documentation.component.ts
+++ src/app/apis/api-documentation/api-documentation.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CardComponent } from '../../components/card/card.component';
 import { RouterLink } from '@angular/router';
+import { CrudNestComponent } from './crud-nest/crud-nest.component';
 
 @Component({
   selector: 'app-api-documentation',
   standalone: true,
-  imports: [RouterLink, CardComponent],
+  imports: [CommonModule, RouterLink, CardComponent, CrudNestComponent],
   templateUrl: './api-documentation.component.html',
   styleUrl: './api-documentation.component.scss',
 })
