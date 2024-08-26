291be141dc7887df6cba14cbcdbeb00c2e5cf6e5
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-26 17:55:34 -0400

    refactor: Update intro-section component and remove unused carousel component

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,11 +1,53 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss',
+  styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent {}
+export class CarouselComponent implements OnInit, OnDestroy {
+  currentIndex = 0;
+  intervalId: any;
+  readonly interval = 4000; // 4 seconds
+  items = [
+    {
+      src: 'https://th.bing.com/th/id/OIG4.Xi5a.97_4DiriMNgneVr?pid=ImgGn',
+      alt: 'Technology bridge',
+    },
+    {
+      src: 'https://th.bing.com/th/id/OIG1.iJdYUNuvyFcoV1v7nyzD?pid=ImgGn',
+      alt: 'Scalable application',
+    },
+    {
+      src: 'https://th.bing.com/th/id/OIG4.z2QSfBUS1FBYeA_PAN55?w=1024&h=1024&rs=1&pid=ImgDetMain',
+      alt: 'Innovative technology',
+    },
+    {
+      src: 'https://th.bing.com/th/id/OIG1.IJeBxVtf0Tmcv85b6_Wn?w=1024&h=1024&rs=1&pid=ImgDetMain',
+      alt: 'Future technology',
+    },
+  ];
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, this.interval);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  showSlide(index: number) {
+    this.currentIndex = index;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.items.length;
+  }
+}
