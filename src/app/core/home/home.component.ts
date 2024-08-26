13bdcac12c469b63a7439385e1fabde3f9819c65
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-26 16:07:14 -0400

    ```text refactor: Update intro-section component
    Add intro-section component with basic HTML structure and SCSS file. This component will be used to display introductory content on the home page.

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
+import { IntroSectionComponent } from './intro-section/intro-section.component';
+i;
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent],
+  imports: [CommonModule, FooterComponent, IntroSectionComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
---
+++ src/app/core/home/intro-section/intro-section.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './intro-section.component.html',
+  styleUrl: './intro-section.component.scss',
+})
+export class IntroSectionComponent {}
