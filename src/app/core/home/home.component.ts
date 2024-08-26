35db02a5c41ebcd1d08ed8130864f079c3fce76f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-26 15:22:58 -0400

    refactor: Remove unused intro component and update home component imports

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
-import { IntroComponent } from './intro/intro.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent, IntroComponent],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
